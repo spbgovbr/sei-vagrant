379891ec58b1dd6d0977987a4564cfe9ac17b3d7
Guilherme Andrade Del Cantoni <guilherme.cantoni@planejamento.gov.br>
2016-11-20 22:17:05 -0200

    Ajustes da imagem dos containers Jod, Oracle e MySQL

--- oracle/install.sh
+++ oracle/install.sh
@@ -5,7 +5,8 @@ set -e
 export ORACLE_HOME=/u01/app/oracle/product/11.2.0/xe
 export PATH=$ORACLE_HOME/bin:$PATH
 export ORACLE_SID=XE
-export NLS_LANG=PORTUGUESE_BRAZIL.WE8MSWIN1252
+#export NLS_LANG=PORTUGUESE_BRAZIL.WE8MSWIN1252
+export NLS_LANG=PORTUGUESE_BRAZIL.WE8ISO8859P1
 
 # Inicialização do servidor
 bash /usr/sbin/startup.sh
@@ -14,11 +15,14 @@ bash /usr/sbin/startup.sh
 sqlplus sys/oracle as sysdba @"/tmp/pre-install.sql"
 
 # Restauração das bases de dados do SEI e SIP
-imp system/oracle file=/tmp/sei.dmp full=y
-imp system/oracle file=/tmp/sip.dmp full=y
+impdp system/oracle file=/tmp/sei_oracle.dmp full=y
+impdp system/oracle file=/tmp/sip_oracle.dmp full=y
 
 # Configuração das bases de dados do sistema
 sqlplus sei_user/sei_user @"/tmp/sei-config.sql"
 sqlplus sip_user/sip_user @"/tmp/sip-config.sql"
+
+# Remover arquivos temporários
+rm -rf /tmp/*
 
 exit 0
