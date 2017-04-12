d111449ccb0caf4b4bbbb1052b15761961558d35
Guilherme Andrade Del Cantoni <guilherme.cantoni@planejamento.gov.br>
2017-04-12 14:38:36 -0300

    Instalação do pacote de fontes do windows no container do Apache Solr

--- solr/install.sh
+++ solr/install.sh
@@ -6,6 +6,12 @@ set -e
 #apk update && apk add lsof curl bash openjdk8-jre
 yum -y update && yum -y install lsof curl wget java-1.8.0-openjdk
 
+# Instalação de pacote de fontes do windows
+rpm -Uvh /tmp/msttcore-fonts-2.0-3.noarch.rpm
+
+# Configuração do pacote de línguas pt_BR
+localedef pt_BR -i pt_BR -f ISO-8859-1
+
 # Download do Solr, versão 6.1.0
 SOLR_URL=https://archive.apache.org/dist/lucene/solr/6.1.0/solr-6.1.0.tgz
 wget -nv $SOLR_URL -O /tmp/solr-6.1.0.tgz
