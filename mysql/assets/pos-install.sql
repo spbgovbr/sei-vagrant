ab4a338df38bd821f0b9a61223358b3be1ace23d
Guilherme Cantoni <guilhermeadc@gmail.com>
2019-09-02 22:03:30 -0300

    Ajuste em scripts de pre-install

--- mysql/assets/pos-install.sql
+++ mysql/assets/pos-install.sql
@@ -2,11 +2,10 @@
 USE sei;
 update orgao set sigla='ABC', descricao='ORGAO ABC' where id_orgao=0;
 delete from auditoria_protocolo;
-update orgao set sin_autenticar='N' where id_orgao=0;
 
 USE sip;
-update orgao set sigla='ABC', descricao='ORGAO ABC' where id_orgao=0;
+update orgao set sigla='ABC', descricao='ORGAO ABC', sin_autenticar='N' where id_orgao=0;
 update sistema set pagina_inicial='http://localhost/sip' where sigla='SIP';
 update sistema set pagina_inicial='http://localhost/sei/inicializar.php', web_service='http://localhost/sei/controlador_ws.php?servico=sip' where sigla='SEI';
 
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'root' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
